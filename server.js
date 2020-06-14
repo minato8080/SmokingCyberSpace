8f556f17475fc3acbbbf83d7154caf2f804f1697
minato8080 <sokonashi0124@gmail.com>
2020-06-14 23:33:15 +0900

    test

---
+++ server.js
@@ -0,0 +1,290 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+const fs = require('fs');
+const options = {
+    flag: 'a',
+    encoding: "utf8"
+};
+const fps = 24;
+let players = {};
+//(ディスプレイ幅/2-遊び)/移動速度
+//(1750-480)/3=423;
+const ZeroPosition = 425;
+//(元画像横幅-ディスプレイ幅/2-遊び)/移動速度
+//(6090-1750-480)/3=1286
+const MaxPosition = 1285;
+
+class GameObject {
+    constructor(obj = {}) {
+        this.id = Math.floor(Math.random() * 100000000);
+        this.x = obj.x;
+        this.y = obj.y;
+        this.width = obj.width;
+        this.height = obj.height;
+    }
+    move(distance) {
+        const oldX = this.x;
+
+        this.x += distance;
+
+        let collision = false;
+        if (this.x < -ZeroPosition || this.x >= MaxPosition) {
+            collision = true;
+        }
+
+        if (collision) {
+            this.x = oldX;
+        }
+        return !collision;
+    }
+    intersect(obj) {
+        return (this.x <= obj.x + obj.width) &&
+            (this.x + this.width >= obj.x);
+    }
+    toJSON() {
+        return { id: this.id, x: this.x, y: this.y, width: this.width, height: this.height,};
+    }
+};
+
+class Player extends GameObject {
+    constructor(obj = {}) {
+        super(obj);
+        this.socketId = obj.socketId;
+        //if (obj.nickname === '') this.nickname = '\u540d\u7121\uff7c';
+        if (obj.nickname === '') this.nickname = 'anonymous';
+        else { this.nickname = obj.nickname;}
+        this.width = 480;
+        this.height = 480;
+        this.x = 0;
+        this.y = 480;
+        this.angle = 1;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.speed = 5;
+        //this.bullets = {};
+        this.isMove = false;
+        this.movement = {};
+
+        this.msg = '';
+        this.msgCountDown = 0;
+
+        this.isSmokeAction = false;
+        this.smokeActionCountDown = 0;
+        this.smokeActionFrame = 0;
+
+        this.isSmoking = false;
+        this.smokingCountDown = 0;
+        this.smokingFrame = 0;
+    }
+    /*
+    shoot() {
+        if (Object.keys(this.bullets).length >= 3) {
+            return;
+        }
+        const bullet = new Bullet({
+            x: this.x + this.width / 2,
+            y: this.y + this.height / 2,
+            angle: this.angle,
+            player: this,
+        });
+        bullet.move(this.width / 2);
+        this.bullets[bullet.id] = bullet;
+        bullets[bullet.id] = bullet;
+    }
+    damage() {
+        this.health--;
+        if (this.health === 0) {
+            this.remove();
+        }
+    }*/
+    /*
+    remove() {
+        delete players[this.id];
+        io.to(this.socketId).emit('dead');
+    }
+    */
+    toJSON() {
+        return Object.assign(super.toJSON(), {
+            socketId: this.socketId, nickname: this.nickname, msg: this.msg,
+            angle: this.angle, isMove: this.isMove,
+            frameX: this.frameX, frameY: this.frameY,
+            isSmoking: this.isSmoking, isSmokeAction: this.isSmokeAction,
+            smokeActionFrame: this.smokeActionFrame, smokingFrame: this.smokingFrame,
+        });
+    }
+};
+/*class Bullet extends GameObject {
+    constructor(obj) {
+        super(obj);
+        this.width = 15;
+        this.height = 15;
+        this.player = obj.player;
+    }
+    remove() {
+        delete this.player.bullets[this.id];
+        delete bullets[this.id];
+    }
+};*/
+
+//let bullets = {};
+
+io.on('connection', function (socket) {
+    let player = null;
+    socket.on('game-start', (config) => {
+        player = new Player({
+            socketId: socket.id,
+            nickname: config.nickname,
+        });
+        players[player.id] = player;
+        //さんが入室しました。
+        /*fs.writeFile("log.txt", LogWriter(player) + '\u3055\u3093\u304c\u5165\u5ba4\u3057\u307e\u3057\u305f\u3002' + '\n', options, (err) => {
+            if (err) { console.log(err); throw err;}
+        });*/
+    });
+    //------------------------------------
+    //ユーザーアクション
+    socket.on('movement', function (movement,isMove) {
+        if (!player) { return; }
+        if (!player.isSmokeAction) {
+            player.movement = movement;
+            if (movement > 0) { player.angle = 1; } else if (movement < 0) { player.angle = -1; }
+            player.isMove = isMove;
+        } else {
+            player.movement.right = false;
+            player.movement.left = false;
+            player.isMove = false;
+        }
+    });
+    socket.on('smoke', function () {
+        if (!player || player.isSmoking) { return; }
+        player.movement.right = false;
+        player.movement.left = false;
+        player.isMove = false;
+        player.isSmokeAction = true;
+        player.isSmoking = true;
+        player.smokeActionCountDown = 6*fps;
+    });
+    
+    //チャット処理
+    socket.on('message', function (msg) {
+        //console.log('message: ' + msg);
+        player.msg = msg;
+        player.msgcount = 400;
+        if(msg !== '')fs.writeFile("log.txt", LogWriter(player) + msg + '\n', options, (err) => {
+            if (err) {
+                console.log(err)
+                throw err
+            }
+        });
+
+        //io.emit('message', msg);
+    });
+    socket.on('disconnect', () => {
+        if (!player) { return; }
+        //さんが退出しました。
+        /*fs.writeFile("log.txt", LogWriter(player) + '\u3055\u3093\u304c\u9000\u51fa\u3057\u307e\u3057\u305f\u3002' + '\n', options, (err) => {
+            if (err) { console.log(err); throw err;}
+        });*/
+        delete players[player.id];
+        player = null;
+    });
+});
+
+
+setInterval(() => {
+    Object.values(players).forEach((player) => {
+        const movement = player.movement;
+        /*if (movement.forward) {
+            player.move(5);
+        }
+        if (movement.back) {
+            player.move(-5);
+        }*/
+        if (movement.left) {
+            player.move(-player.speed);
+            player.angle = -1;
+        }
+        if (movement.right) {
+            player.move(player.speed);
+            player.angle = 1;
+        }
+        if (player.msgCountDown > 0) {
+            player.msgCountDown--;
+            if (player.msgCountDown === 0) player.msg = '';
+        }
+        if (player.smokeActionCountDown > 0) {
+            player.smokeActionCountDown--;
+            if (player.smokeActionCountDown % 6 === 0) player.smokeActionFrame++;
+            if (player.smokeActionCountDown === 0) {
+                player.isSmokeAction = false;
+                player.smokeActionFrame = 0;
+                player.smokingCountDown = 40*fps;
+            }
+        }
+        if (player.smokingCountDown > 0) {
+            player.smokingCountDown--;
+            if (player.smokingCountDown % 6 === 0) player.smokingFrame++;
+            if (player.smokingCountDown === 0) player.isSmoking = false;
+        }
+    });
+    /*
+    Object.values(bullets).forEach((bullet) => {
+        if (!bullet.move(10)) {
+            bullet.remove();
+            return;
+        }
+        Object.values(players).forEach((player) => {
+            if (bullet.intersect(player)) {
+                if (player !== bullet.player) {
+                    player.damage();
+                    bullet.remove();
+                    bullet.player.point += 1;
+                }
+            }
+        });
+    });*/
+    io.sockets.emit('state', players);
+}, 1000 / fps);
+
+
+//-----------------------------
+//ログ関連
+let LogWriter = function (player) {
+    let date = new Date();
+    let yyyy = date.getFullYear();
+    let mm = toDoubleDigits(date.getMonth() + 1);
+    let dd = toDoubleDigits(date.getDate());
+    let hh = toDoubleDigits(date.getHours());
+    let mi = toDoubleDigits(date.getMinutes());
+    let ss = toDoubleDigits(date.getSeconds());
+    let time = yyyy + '/' + mm + '/' + dd + ' ' + hh + ':' + mi + ':' + ss;
+    //let log = player.msg + '\t'+time + ' id:' + player.id + ' name:' + player.nickname +   '\n';
+    let log = time + '^ID:' + player.id.toString(32) + '@' + player.nickname + '<';
+    return log;
+}
+let toDoubleDigits = function (num) {
+    num += "";
+    if (num.length === 1) {
+        num = "0" + num;
+    }
+    return num;
+};
+
+
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, '/static/index.html'));
+});
+
+server.listen(process.env.PORT || 3000, function () {
+    console.log('Starting server on port 3000');
+});
---
+++ static/game.js
@@ -0,0 +1,447 @@
+﻿'use strict';
+
+const socket = io();
+const canvas = $('#canvas-2d')[0];
+const mapImage = $('#map-image')[0];
+const mapImageZ1 = $('#map-image-z1')[0];
+const Controler = $('#controler')[0];
+const Pixer = $('#pixer')[0];
+const context = canvas.getContext('2d');
+const playerImage = $('#player-image')[0];
+const chatboxTop = $('#top')[0];
+const chatboxMiddle = $('#middle')[0];
+const chatboxUnder = $('#under')[0];
+
+let movement = {};
+let myplayerpos;
+let movespeed = 3;
+let DisplayTop = 0;
+let DisplayCenter = 1750;
+let isMove = false;
+
+
+
+//------------------------------------
+//           スタート処理
+//------------------------------------
+
+function gameStart() {
+    socket.emit('game-start', { nickname: $("#nickname").val() });
+    $("#start-screen").hide();
+}
+$("#start-button").on('click', gameStart);
+
+
+
+//-------------------------------------
+//　　　　　　通信処理
+//-------------------------------------
+
+$(document).on('keydown keyup', (event) => {
+    const KeyToCommand = {
+       // 'ArrowUp': 'forward',
+       // 'ArrowDown': 'back',
+        'ArrowLeft': 'left',
+        'ArrowRight': 'right',
+    };
+    const command = KeyToCommand[event.key];
+    if (command) {
+        if (event.type === 'keydown') {
+            movement[command] = true;
+            isMove = true;
+        } else { /* keyup */
+            movement[command] = false;
+            isMove = false;
+        }
+        socket.emit('movement', movement,isMove);
+    }
+    if (event.key === 'a' && event.type === 'keydown') {
+        socket.emit('smoke');
+    }
+});
+
+socket.on('state', function (players, ) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.lineWidth = 10;
+    context.beginPath();
+    context.fillStyle = "rgb(62,12,15)"
+   // context.rect(0, 0, canvas.width, canvas.height);
+    context.fill();
+    context.stroke();
+    //自プレイヤー
+    Object.values(players).forEach((player) => {
+        if (player.socketId === socket.id) {
+            myplayerpos = player.x;
+            context.save();
+            //自分視点の背景を描画
+            context.drawImage(mapImage, -myplayerpos * movespeed, +DisplayTop);
+            context.font = textfont;
+            context.fillStyle = textcolor;
+            ChatWriter(player);
+            NameWriter(player);
+            //自プレイヤーを描画
+            PlayerFrameChanger(player);
+            context.drawImage(playerImage, player.frameX, player.frameY, player.width, player.height,
+                DisplayCenter-240, 960 - player.height - 10 + DisplayTop, 480, 480);
+            context.restore();
+        }
+    });
+    //他プレイヤー
+    Object.values(players).forEach((player) => {
+        if (player.socketId !== socket.id) {
+            context.save();
+            context.font = textfont;
+            context.fillStyle = textcolor;
+            ChatWriter(player);
+            NameWriter(player);
+            //他プレイヤーを描画
+            PlayerFrameChanger(player);
+            context.drawImage(playerImage, player.frameX, player.frameY, player.width, player.height,
+                (player.x - myplayerpos) * movespeed + DisplayCenter -240, 960 - player.height + DisplayTop, 480, 480);
+            context.restore();
+        }
+    });
+
+    context.drawImage(mapImageZ1, -myplayerpos * movespeed, +DisplayTop);
+   // context.drawImage(Controler, DisplayCenter - 750, +DisplayTop + 1050);
+    //ピクセルカウント用
+    //context.drawImage(Pixer, 0,0);
+
+    /*
+    Object.values(bullets).forEach((bullet) => {
+        context.beginPath();
+        context.arc(bullet.x, bullet.y, bullet.width / 2, 0, 2 * Math.PI);
+        context.stroke();
+    });*/
+});
+
+socket.on('dead', () => {
+    $("#start-screen").show();
+});
+
+
+
+//---------------------------------
+//         チャット関連
+//---------------------------------
+
+
+//----------------------------------
+//          チャットI/O
+
+$(function () {
+    $('#message_form').submit(function () {
+
+        socket.emit('message', $('#input_msg').val());
+        $('#input_msg').val('');
+        return false;
+    });
+});
+$('#sousin').on('click', function () {
+    socket.emit('message', $('#input_msg').val());
+    $('#input_msg').val('');
+    return false;
+});
+
+//----------------------------------
+//          チャット描画
+//----------------------------------
+
+//let textcolor = "rgb(180,104,100)";
+//let textcolor = "rgb(64,26,36)";
+//let textcolor = "rgb(72,58,64)";
+let textcolor = "rgb(88,106,76)";
+//let textcolor = "rgb(176,104,76)";
+let textfont = '40px misaki2';
+let margin = 60;
+let oneline = 6;
+let num, len, lines, chr;
+let bufferSize = 0;
+let buffer = '';
+let chatarray = [];
+
+let ChatWriter = function (player) {
+    if (player.msg === '') {return}
+    if (player.socketId !== socket.id) {
+        num = (player.x - myplayerpos) * movespeed;
+    } else {
+        num = 0;
+    }
+
+    len = player.msg.length;
+    lines = Math.ceil(len / oneline);
+    for (let i = 0 ,bufferLength = 0; i < len; i++) {
+        buffer += player.msg.charAt(i);
+        chr = buffer.charCodeAt(bufferLength);
+        if ((chr >= 0x00 && chr < 0x81) ||
+            (chr === 0xf8f0) ||
+            (chr >= 0xff61 && chr < 0xffa0) ||
+            (chr >= 0xf8f1 && chr < 0xf8f4)) {
+            //半角文字の場合は0.5を加算
+            bufferSize += 0.5;
+        } else {
+            //それ以外の文字の場合は1を加算
+            bufferSize += 1.0;
+        }
+        bufferLength++
+        if (bufferSize >= oneline && i !== len - 1) {
+            chatarray.push(buffer);
+            buffer = '';
+            bufferSize = 0;
+            bufferLength = 0;
+        }
+        
+    }
+    chatarray.push(buffer);
+    buffer = '';
+    bufferSize = 0;
+
+    context.drawImage(chatboxMiddle, 0, 0, 290, 35,
+        num + DisplayCenter - (oneline * 20)-20, player.y - margin - 35  - chatarray.length * 40 + textfloater, 290, chatarray.length * 40);
+    context.drawImage(chatboxTop, num + DisplayCenter - (oneline * 20)-20, player.y - margin - chatarray.length * 40 - 30 - 15 - 5 + textfloater);
+    context.drawImage(chatboxUnder, num + DisplayCenter - (oneline * 20) -20, player.y - margin -30 -5 + textfloater);
+
+    for (let i = 0; i < chatarray.length; i++) {
+        context.fillText(chatarray[i], num + DisplayCenter - oneline * 20, player.y - margin + DisplayTop - (chatarray.length - i) * 40 + textfloater);
+    }
+    chatarray = [];
+}
+
+let NameWriter = function (player) {
+    if (player.socketId !== socket.id) {
+        num = (player.x - myplayerpos) * movespeed;
+    } else {
+        num = 0;
+    }
+    for (let i = 0; i < player.nickname.length; i++) {
+        chr = player.nickname.charCodeAt(i);
+        if ((chr >= 0x00 && chr < 0x81) ||
+            (chr === 0xf8f0) ||
+            (chr >= 0xff61 && chr < 0xffa0) ||
+            (chr >= 0xf8f1 && chr < 0xf8f4)) {
+            //半角文字の場合は0.5を加算
+            bufferSize += 0.5;
+        } else {
+            //それ以外の文字の場合は1を加算
+            bufferSize += 1.0;
+        }
+    }
+    context.fillText('＠' + player.nickname, num + DisplayCenter - (Math.ceil(bufferSize) + 1) * 20, player.y - 30 + DisplayTop + textfloater);
+    bufferSize = 0;
+}
+
+
+//----------------------------
+//      フレーム更新
+//----------------------------
+let age = 0;
+let frame4_1fps = 0;
+let frame8_4fps = 0;
+let textfloater = 0;
+setInterval(() => {
+    age++;
+    frame8_4fps++; {}
+    if (age % 4 === 0) {
+        frame4_1fps++;
+        if (frame4_1fps === 4) {
+            frame4_1fps = 0;
+        }
+        if (frame4_1fps > 1) {
+            textfloater += 5;
+        } else { textfloater -= 5; }
+
+        if (age % 8 === 0) {
+            frame8_4fps = 0;
+        }
+    }
+    
+}, 1000 / 4);
+
+//-----------------------------------
+//    プレイヤーフレーム管理&更新
+//------------------------------------
+
+let PlayerFrameChanger = function (player) {
+    if (player.isSmokeAction) { //喫煙
+        SmokeActionFrameChanger(player);
+    }
+    else if (player.isSmoking) {//喫煙アイドル
+        SmokingFrameChanger(player);
+    }
+    else if (player.isMove) { //歩行
+        WalkFrameChanger(player);
+    }
+    else { // アイドル
+        IdleFrameChanger(player);
+    }
+}
+let SmokeActionFrameChanger = function (player) {
+    if (player.angle < 0) {
+        if (player.smokeActionFrame <= 14) {
+            player.frameY = player.height * 2;
+        } else { player.frameY = player.height * 4; }
+    }else if (player.angle > 0) {
+        if (player.smokeActionFrame <= 14) {
+            player.frameY = player.height * 3;
+        } else { player.frameY = player.height * 5; }
+    }
+
+
+    switch (player.smokeActionFrame) {
+        case 0: player.frameX = 0; break;
+        case 1: player.frameX = player.width * 1; break;
+        case 2: player.frameX = player.width * 2; break;
+        case 3: player.frameX = player.width * 3; break;
+        case 4: player.frameX = player.width * 4; break;
+        case 5: player.frameX = player.width * 5; break;
+        case 6: player.frameX = player.width * 6; break;
+        case 7: player.frameX = player.width * 7; break;
+        case 8: player.frameX = player.width * 6; break;
+        case 9: player.frameX = player.width * 7; break;
+        case 10: player.frameX = player.width * 6; break;
+        case 11: player.frameX = player.width * 7; break;
+        case 12: player.frameX = player.width * 8; break;
+        case 13: player.frameX = player.width * 9; break;
+        case 14: player.frameX = player.width * 3; break;
+        case 15: player.frameX = player.width * 3; break;
+        case 16: player.frameX = player.width * 3; break;
+        case 17: player.frameX = player.width * 3; break;
+        case 18: player.frameX = player.width * 2; break;
+        case 19: player.frameX = player.width * 1; break;
+        case 20: player.frameX = player.width * 4; break;
+        case 21: player.frameX = player.width * 5; break;
+        case 22: player.frameX = player.width * 6; break;
+        case 23: player.frameX = player.width * 7; break;
+    }
+
+}
+let SmokingFrameChanger = function (player) {
+    if (player.angle < 0) {
+            player.frameY = player.height * 4;
+    }
+    else if (player.angle > 0) {
+            player.frameY = player.height * 5;
+    }
+    if (player.isMove) {
+        switch (frame8_4fps) {
+            case 0: case 4: player.frameX = 0; break;
+            case 1: case 5: player.frameX = player.width * 8; break;
+            case 2: case 6: player.frameX = 0; break;
+            case 3: case 7: player.frameX = player.width * 9; break;
+        }
+    }
+    else {
+        switch (player.smokingFrame % 32) {
+            case 0: player.frameX = 0; break;
+            case 1: player.frameX = player.width * 1; break;
+            case 2: player.frameX = player.width * 2; break;
+            case 3: player.frameX = player.width * 3; break;
+            case 4: player.frameX = player.width * 3; break;
+            case 5: player.frameX = player.width * 3; break;
+            case 6: player.frameX = player.width * 3; break;
+            case 7: player.frameX = player.width * 3; break;
+            case 8: player.frameX = player.width * 3; break;
+            case 9: player.frameX = player.width * 3; break;
+            case 10: player.frameX = player.width * 2; break;
+            case 11: player.frameX = player.width * 1; break;
+            case 12: player.frameX = player.width * 0; break;
+            case 13: player.frameX = player.width * 4; break;
+            case 14: player.frameX = player.width * 5; break;
+            case 15: player.frameX = player.width * 6; break;
+            case 16: player.frameX = player.width * 7; break;
+            case 17: case 18: case 19: player.frameX = 0; break;
+            case 20: player.frameX = player.width * 4; break;
+            case 21: player.frameX = player.width * 5; break;
+            case 22: player.frameX = player.width * 6; break;
+            case 23: player.frameX = player.width * 7; break;
+            case 24: case 25: case 26: case 27: player.frameX = 0; break;
+            case 28: player.frameX = player.width * 4; break;
+            case 29: player.frameX = player.width * 5; break;
+            case 30: player.frameX = player.width * 6; break;
+            case 31: player.frameX = player.width * 7; break;
+        }
+    }
+}
+let IdleFrameChanger = function (player) {
+    if (player.angle < 0) {
+        player.frameY = player.height;
+        switch (frame8_4fps) {
+            case 0: case 1: case 2: case 3: player.frameX = 0; break;
+            case 4: player.frameX = player.width * 1; break;
+            case 5: player.frameX = player.width * 2; break;
+            case 6: player.frameX = player.width * 3; break;
+            case 7: player.frameX = player.width * 4; break;
+        }
+    }
+    else if (player.angle > 0) {
+        player.frameY = player.height;
+        switch (frame8_4fps) {
+            case 0: case 1: case 2: case 3: player.frameX = player.width * 5; break;
+            case 4: player.frameX = player.width * 6; break;
+            case 5: player.frameX = player.width * 7; break;
+            case 6: player.frameX = player.width * 8; break;
+            case 7: player.frameX = player.width * 9; break;
+        }
+    }
+}
+let WalkFrameChanger = function (player) {
+    if (player.angle < 0) {
+        player.frameY = 0;
+        switch (frame8_4fps) {
+            case 0: case 4: player.frameX = 0; break;
+            case 1: case 5: player.frameX = player.width * 1; break;
+            case 2: case 6: player.frameX = 0; break;
+            case 3: case 7: player.frameX = player.width * 2; break;
+        }
+    }
+    else if (player.angle > 0) {
+        player.frameY = 0;
+        switch (frame8_4fps) {
+            case 0: case 4: player.frameX = player.width * 3; break;
+            case 1: case 5: player.frameX = player.width * 4; break;
+            case 2: case 6: player.frameX = player.width * 3; break;
+            case 3: case 7: player.frameX = player.width * 5; break;
+        }
+    }
+}
+
+
+
+
+
+
+//------------------------------------
+//       コントローラー
+//------------------------------------
+
+let LKey = function (event) {
+    if (event === 'down') {
+        movement.right = false;
+        movement.left = true;
+        isMove = true;
+    } else {
+        movement.right = false;
+        movement.left = false;
+        isMove = false;
+    }
+    socket.emit('movement', movement, isMove);
+}
+let RKey = function (event) {
+    if (event === 'down') {
+        movement.right = true;
+        movement.left = false;
+        isMove = true;
+    } else {
+        movement.right = false;
+        movement.left = false;
+        isMove = false;
+    }
+    socket.emit('movement', movement, isMove);
+}
+let AKey = function (event) {
+    socket.emit('smoke', movement, isMove);
+}
+
+let BKey = function (event) {
+}
